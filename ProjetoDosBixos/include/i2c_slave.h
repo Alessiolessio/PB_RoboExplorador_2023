e06c9ec56cd52513e969c5155268d65488d8ff98
MatheusPaivaa <matheuzu11@gmail.com>
2023-10-24 17:25:25 -0300

    Organização sugerida por Chicão

--- ProjetoDosBixos/include/i2c_slave.h
+++ ProjetoDosBixos/include/i2c_slave.h
@@ -8,8 +8,6 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 
-static const char *TAG = "i2c-teste";
-
 // Definição dos parâmetros do barramento I2C
 #define I2C_SLAVE_NUM I2C_NUM_0
 #define I2C_SLAVE_SCL_IO 22 // Pino SLC da ESP32
@@ -18,17 +16,102 @@ static const char *TAG = "i2c-teste";
 #define I2C_SLAVE_RX_BUF_LEN 256
 #define I2C_SLAVE_ADDRESS 0x08
 
+// Definição dos valores realmente utilizados na escrita e leitura
+#define WRITE_LEN_VALUE 4
+#define READ_LEN_VALUE 6
 #define TIMEOUT_MS 1000
+
+// Definição de valores para empacotamento de desempacotamento
 #define LAST_BYTE_MASK 0xFF
 
-static QueueHandle_t i2c_write_queue = NULL;
+// Definição de frequências utilizadas dentro das tasks
+#define FREQ_COMMUNICATION 1000
+#define FREQ_CONTROL 2000
+
+static QueueHandle_t i2c_write_queue = NULL; 
+static const char *TAG = "i2c-teste"; // TAG utilizada para dar display no console
 
 // Protótipo de funções:
+
+/**
+ * @brief Inicializa o driver do barramento I2C no modo escravo
+ *
+ * Esta função inicializa o driver I2C no modo escravo, configurando os pinos SDA e SCL,
+ * definindo a configuração do barramento I2C e instalando o driver.
+ *
+ * @return
+ *     - ESP_OK: O driver I2C foi inicializado com sucesso.
+ *     - ESP_ERR_INVALID_ARG: Falha na inicialização devido a argumentos inválidos.
+ *     - ESP_ERR_NO_MEM: Falha na inicialização devido à falta de memória disponível.
+ *     - ESP_FAIL: Outros erros na inicialização do driver I2C.
+ */
 static esp_err_t i2c_slave_init(void);
-static void display_data(uint8_t *data, int len);
+
+/**
+ * @brief Tarefa de leitura dos dados do barramento I2C
+ *
+ * Esta função é executada como uma tarefa e lê os dados do barramento I2C,
+ * desempacota os valores lidos e os retorna. Os valores são enviados por outra tarefa.
+ *
+ * @param params Parâmetros da tarefa (não utilizados).
+ * @return O valor lido a partir do barramento I2C.
+ */
 static int i2c_read_task(void *params);
-char *int_to_string(int value);
+
+/**
+ * @brief Tarefa de escrita de dados no barramento I2C
+ *
+ * Esta função é executada como uma tarefa e envia os dados recebidos por outra tarefa
+ * para o barramento I2C. Os valores são empacotados antes do envio.
+ *
+ * @param params Parâmetros da tarefa (não utilizados).
+ * @param value O valor a ser escrito no barramento I2C.
+ */
 static void i2c_write_task(void *params, int value);
+
+/**
+ * @brief Tarefa de comunicação e escrita no barramento I2C
+ *
+ * Esta função é executada como uma tarefa e é responsável por coordenar a comunicação
+ * entre a leitura e escrita de dados no barramento I2C. Ela lê os valores, aguarda,
+ * e então escreve os valores lidos no barramento.
+ *
+ * @param params Parâmetros da tarefa (não utilizados).
+ */
 static void i2c_task_com(void *pvParameters);
+
+/**
+ * @brief Tarefa de controle
+ *
+ * Esta função é executada como uma tarefa e lida com o controle do motor e outros
+ * aspectos relacionados. Os valores lidos do barramento I2C são utilizados aqui para
+ * controle.
+ *
+ * @param params Parâmetros da tarefa (não utilizados).
+ */
 static void i2c_task_controle(void *pvParameters);
-static esp_err_t create_tasks(int parametros);
+
+/**
+ * @brief Cria tarefas para leitura e escrita de dados no barramento I2C
+ *
+ * Esta função cria as tarefas para leitura e escrita de dados no barramento I2C,
+ * além de inicializar o driver do barramento I2C.
+ *
+ * @param parametros Parâmetros para as tarefas (não utilizados).
+ * @param display Flag para controle de exibição.
+ * @return
+ *     - ESP_OK: As tarefas foram criadas com sucesso.
+ *     - Outros códigos de erro em caso de falha.
+ */
+static esp_err_t create_tasks(int parametros, int display);
+
+/**
+ * @brief Exibe dados em formato hexadecimal
+ *
+ * Esta função exibe os dados em formato hexadecimal.
+ *
+ * @param data Ponteiro para os dados a serem exibidos.
+ * @param len Tamanho dos dados.
+ */
+static void display_data(uint8_t *data, int len);
+
--- ProjetoDosBixos/src/i2c_slave.c
+++ ProjetoDosBixos/src/i2c_slave.c
@@ -1,13 +1,11 @@
 #include "i2c_slave.h"
 
-int valor = 0; 
+int valor = 0; // GLobal variable for testing communincation intercores
 
-// Inicializa o I2C escravo
 static esp_err_t i2c_slave_init(void) {
     
     int i2c_slave_port = I2C_SLAVE_NUM;
 
-    // Configuração da struct do barramento I2C
     i2c_config_t conf_slave = {
         .sda_io_num = I2C_SLAVE_SDA_IO,         
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
@@ -18,46 +16,29 @@ static esp_err_t i2c_slave_init(void) {
         .slave.slave_addr = I2C_SLAVE_ADDRESS,
     };
 
-    // Configuração ods parâmetros do barramento I2C com base nas configurações definidas na estrutura conf_slave
-    esp_err_t err = i2c_param_config(i2c_slave_port, &conf_slave); // Caso dê tudo certo retorn ESP_OK
+    esp_err_t err = i2c_param_config(i2c_slave_port, &conf_slave);
 
-    // Captura do erro, caso a config dê errado
     if (err != ESP_OK) {
         return err;
     }
     
-    // Instalar (configurar + inicializar) o driver I2C
     return i2c_driver_install(i2c_slave_port, conf_slave.mode, I2C_SLAVE_RX_BUF_LEN, I2C_SLAVE_TX_BUF_LEN, 0);
 }
 
-// Função para printar os dados na tela
-static void display_data(uint8_t *data, int len) {
-    int i;
-    for (i = 2; i < len; i++) {
-        printf("%02x ", data[i]);
-
-        if ((i + 1) % 16 == 0) { // A cada 16 elementos quebramos uma linha (melhorar vizualização)
-            printf("\n");
-        }
-    }
-    printf("\n");
-}
-
-// Função para a leitura dos valores enviados da Master
 static int i2c_read_task(void *params) {
 
     int x = 0;
 
     uint8_t rx_data[I2C_SLAVE_RX_BUF_LEN];
 
-    int size = i2c_slave_read_buffer(I2C_SLAVE_NUM, rx_data, 6, TIMEOUT_MS / portTICK_PERIOD_MS);
+    int size = i2c_slave_read_buffer(I2C_SLAVE_NUM, rx_data, READ_LEN_VALUE, TIMEOUT_MS / portTICK_PERIOD_MS);
 
+    // "Unpacking" valores
     for(int i = 2; i < size; i++){
-        x |= (rx_data[i] << 8*(5-i));
+        x |= (rx_data[i] << 8 * ((READ_LEN_VALUE - 1) - i));
     } 
 
     if (size > 0) {
-        //printf("Valor lido: %d \n", x);
         valor = x;
         return x;
 
@@ -68,46 +49,37 @@ static int i2c_read_task(void *params) {
     return 0;
 }
 
-// Tranforma o int para uma string
-char *int_to_string(int value) {
-    char *str = (char *)malloc(12);  // Espaço suficiente para armazenar um inteiro com sinal de 32 bits
-    snprintf(str, 12, "%d", value);
-    return str;
-}
-
-// Função para converter um inteiro para uma sequência de caracteres (string)
 static void i2c_write_task(void *params, int value) {
-    // Converte a sequência de caracteres (string) em uint8_t
-    uint8_t tx_data[4];
+    uint8_t tx_data[WRITE_LEN_VALUE];
 
-    for (int i = 0; i < 4; i++) {
-        tx_data[i] = (value >> 8 * (3 - i)) & LAST_BYTE_MASK;
+    // "Packing" valores
+    for (int i = 0; i < WRITE_LEN_VALUE; i++) {
+        tx_data[i] = (value >> 8 * ((WRITE_LEN_VALUE - 1) - i)) & LAST_BYTE_MASK;
     }
 
-    int size = i2c_slave_write_buffer(I2C_SLAVE_NUM, tx_data, 4, TIMEOUT_MS / portTICK_PERIOD_MS);
+    int size = i2c_slave_write_buffer(I2C_SLAVE_NUM, tx_data, WRITE_LEN_VALUE, TIMEOUT_MS / portTICK_PERIOD_MS);
 
     if (size > 0) {
-        //printf("Valor escrito: %d \n", value);
+        printf("Valor escrito: %d \n", value);
     } else {
         ESP_LOGI(TAG, "Escrita falhou!");
     }
     
 }
 
-// Função que une as funções de read e write (modularizadas), em sequência
 static void i2c_task_com(void *params) {
 
-    int x = 0;
+    int valor_lido = 0;
 
     while(1){
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(FREQ_COMMUNICATION / portTICK_PERIOD_MS);
 
-        x = i2c_read_task(NULL);
+        valor_lido = i2c_read_task(NULL);
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(FREQ_COMMUNICATION / portTICK_PERIOD_MS);
 
-        i2c_write_task(params, x);
+        i2c_write_task(params, valor_lido);
 
     }
 
@@ -117,18 +89,18 @@ static void i2c_task_controle(void *params) {
 
     while(1){
 
+        // Implementar baixo nível do controle
         ESP_LOGI(TAG, "Task 2:  %d\n", valor);
 
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        vTaskDelay(FREQ_CONTROL / portTICK_PERIOD_MS);
 
     }
 
 }
 
-// Utiliza o FreeRTOS para distribuir as tarefas entre os cores 0 e 1
-static esp_err_t create_tasks(int parametros) {
+static esp_err_t create_tasks(int parametros, int display) {
 
-    ESP_ERROR_CHECK(i2c_slave_init()); // Inicializa o Slave e confere se nenhum erro foi capturado
+    ESP_ERROR_CHECK(i2c_slave_init());
 
     i2c_write_queue = xQueueCreate(10, I2C_SLAVE_TX_BUF_LEN);
 
@@ -136,16 +108,21 @@ static esp_err_t create_tasks(int parametros) {
     xTaskCreatePinnedToCore(i2c_task_com, "i2c_task_com", 2048, &parametros, 5, NULL, 0);
 
     // Task 2 (core 1): controle do motor e derivados
-
     xTaskCreatePinnedToCore(i2c_task_controle, "i2c_task_controle", 2048, NULL, 5, NULL, 1);
 
-    // Caso as tasks sejam criadas, retorna sucesso
     return ESP_OK;
 }
 
-void app_main(void) {
+static void display_data(uint8_t *data, int len) {
+    int i;
+    for (i = 2; i < len; i++) {
+        printf("%02x ", data[i]);
 
-    int parametro_teste = 1;
-
-    ESP_ERROR_CHECK(create_tasks(parametro_teste)); // Utiliza o FreeRTOS para criar as tasks em diferentes cores
+        if ((i + 1) % 16 == 0) {
+            printf("\n");
+        }
+    }
+    printf("\n");
 }
+
+
