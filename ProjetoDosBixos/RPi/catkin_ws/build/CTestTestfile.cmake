5af849387e08941ea2c6358aa101f0daa662e230
MatheusPaivaa <matheuzu11@gmail.com>
2023-11-13 17:22:49 -0300

    Começo ROS

---
+++ ProjetoDosBixos/RPi/catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/src
+# Build directory: /home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robot_control")
+subdirs("robot_description")
