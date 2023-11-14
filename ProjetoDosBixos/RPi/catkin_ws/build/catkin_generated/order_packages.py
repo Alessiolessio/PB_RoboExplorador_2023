ecbc7e162c7f5834cfc530d69997c95d8ecabd93
Matheus Paiva <matheuzu11@gmail.com>
2023-11-14 17:28:36 -0300

    Apagando Build

--- ProjetoDosBixos/RPi/catkin_ws/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/devel;/home/math/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/devel;/home/math/catkin_ws/devel;/opt/ros/noetic' != '' else []
