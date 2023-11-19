f82b50ac2404e5a6d024338d1d61fb90ec4fe1df
Matheus Paiva <matheuzu11@gmail.com>
2023-11-19 16:25:51 -0300

    Delete devel

--- ProjetoDosBixos/RPi/catkin_ws/devel/share/robot_control/cmake/robot_control-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(robot_control_MSG_INCLUDE_DIRS "/home/ubuntu/PB_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/src/robot_control/msg")
-set(robot_control_MSG_DEPENDENCIES std_msgs)
