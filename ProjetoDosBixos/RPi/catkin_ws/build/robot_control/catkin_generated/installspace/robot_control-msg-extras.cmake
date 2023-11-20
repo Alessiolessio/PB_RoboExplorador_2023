f484a38b2218cad55ac16de86c75375dd08b083f
Matheus Paiva <matheuzu11@gmail.com>
2023-11-20 13:04:17 -0300

    Delete Pbuild

--- ProjetoDosBixos/RPi/catkin_ws/build/robot_control/catkin_generated/installspace/robot_control-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(robot_control_MESSAGE_FILES "msg/encoder_data.msg;msg/velocity_data.msg")
-set(robot_control_SERVICE_FILES "")
--- ProjetoDosBixos/RPi/catkin_ws/build/ros_controllers/diff_drive_controller/setup_custom_pythonpath.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/devel/lib/python3/dist-packages:$PYTHONPATH
-exec /usr/bin/python3 "$@"
