c00b569730f4e01c2e0dd1bc7177f8d6320b4b58
MatheusPaivaa <matheuzu11@gmail.com>
2023-11-20 13:05:43 -0300

    Hw interface

--- ProjetoDosBixos/RPi/catkin_ws/build/robot_control/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/devel/include".split(';') if "/home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "robot_control"
-PROJECT_SPACE_DIR = "/home/math/Documentos/pb_2023/PB_2023/ProjetoDosBixos/RPi/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- ProjetoDosBixos/RPi/catkin_ws/devel/share/common-lisp/ros/robot_control/msg/_package_encoder_data.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package robot_control-msg)
-(cl:export '(LEFT_ENCODER_DATA-VAL
-          LEFT_ENCODER_DATA
-          RIGHT_ENCODER_DATA-VAL
-          RIGHT_ENCODER_DATA
-))
